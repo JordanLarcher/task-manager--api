3bdc559661b8a8b366b7d262df948dfac3400b13
jordan larcher <jordan.larcher.lds@gmail.com>
2025-05-25 17:17:28 -0300

    updating mongo connection

--- config/database.js
+++ config/database.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
+const winston = require('winston');
 
-module.exports = () => mongoose.connect(process.env.MONGO_URI);
+
+const connectDB = async () =>  {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        winston.info('Connected to Mongo database');
+    } catch (err) {
+        winston.error(`Failed to connect to database: ${err.message}`);
+        throw err;
+    }
+}
